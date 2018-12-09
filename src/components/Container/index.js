d28746c5846c12760baa5c4885beb55d743af35c
Milene Vieira Lacerda <milenelacerda@icloud.com>
2018-12-09 15:04:55 -0200

    Modifying container css values

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
-  max-width: ${({ theme }) => theme.container.maxWidth};
-  width: 100%;
+  width: 1090px;
+  position: relative;
   margin: 0 auto;
   padding: 0 ${({ theme }) => theme.container.gutter};
 `;
