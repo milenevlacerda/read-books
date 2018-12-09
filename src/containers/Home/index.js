344f2ecac15bf840c765065f40518c121cabe59a
Milene Vieira Lacerda <milenelacerda@icloud.com>
2018-12-09 15:05:29 -0200

    Create Home component js

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 
 import Container from '@/components/Container';
 import Head from '@/components/Head';
+import Header from '@/components/Header';
+import Card from '@/components/Card';
+import Footer from '@/components/Footer';
 
 export default function Home() {
   return (
-    <Container>
-      <Head title="Home" />
-    </Container>
+    <div>
+      <Header/>
+      <Container>
+        <Head title="Home" />
+        <Card/>
+      </Container>
+      <Footer/>
+    </div>    
   );
 }
