bdb2a165b6c72bc86da8f65f9593f45a83f19f14
Milene Vieira Lacerda <milenelacerda@icloud.com>
2018-12-09 15:04:32 -0200

    Create CardExpanded component js

---
+++ src/components/CardExpanded/index.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import CapaLivro from '@/assets/images/2018.10-aberto-ineditos.jpg';
+import LogoTag from '@/assets/images/logo.png';
+import LogoGoodReads from '@/assets/images/goodreads.png';
+import Rating from '@/assets/images/rating.png';
+import BackButton from '../BackButton/index';
+
+export default function() {
+  return (
+    <div>
+      <BackButton />
+      <div className="card-expanded-box">
+        <div className="card-expanded">
+          <div className="card-header">
+            <img className="book-image" src={CapaLivro} alt="" />
+          </div>
+
+          <div className="card-content">
+            <h3 className="card-title">A Filha do Rei do Pântano</h3>
+
+            <div className="book-details">
+              <p className="card-description">
+                <strong className="description-item">Autora </strong>
+                Keren Dionne
+            </p>
+              <p className="card-description">
+                <strong className="description-item">Edição </strong>
+                08/2018
+            </p>
+              <p className="card-description">
+                <strong className="description-item">Curador </strong>
+                Milene V. Lacerda
+            </p>
+              <p className="card-description">
+                <strong className="description-item">Nº de Páginas </strong>
+                240 Páginas
+            </p>
+              <p className="card-description">
+                <strong className="description-item">Nº de Páginas </strong>
+                240 Páginas
+            </p>
+              <p className="card-description">
+                <strong className="description-item">
+                  Nº de Avaliações TAG{' '}
+                </strong>
+                40 Avaliações
+            </p>
+              <p className="card-description">
+                <strong className="description-item">
+                  Nº de Avaliações GoodReads{' '}
+                </strong>
+                30 Avaliações
+            </p>
+            </div>
+
+            <div className="rating-box">
+              <p className="card-description">
+                <strong className="description-item rating-item">
+                  Média das Avaliações
+              </strong>
+              </p>
+            </div>
+
+            <div className="evaluation-box">
+              <div className="box">
+                <img src={LogoTag} alt="" className="logo" />
+                <span className="grade">5,0</span>
+                <img className="rating" src={Rating} alt="" />
+              </div>
+
+              <div className="box">
+                <img src={LogoGoodReads} alt="" className="logo" />
+                <span className="grade">5,0</span>
+                <img className="rating" src={Rating} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>  
+    </div>
+  );
+};
