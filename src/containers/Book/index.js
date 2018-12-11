4551f17f030ef9d31e7176b4eb450da5d6a0ef89
Milene Vieira Lacerda <milenelacerda@icloud.com>
2018-12-11 10:19:18 -0200

    Add navigate structure to component

--- src/containers/Book/index.js
+++ src/containers/Book/index.js
@@ -1,22 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { navigate } from '@reach/router';
 
 import Container from '@/components/Container';
 import Head from '@/components/Head';
 import Header from '@/components/Header';
 import CardExpanded from '@/components/CardExpanded/index';
 import Footer from '@/components/Footer';
+import BackButton from '@/components/BackButton/index';
 
-
-
-export default function Home() {
+function Book({ isbn, books }) {
   return (
     <div>
       <Header/>
       <Container>
         <Head title="Home" />
-        <CardExpanded/>
+        <BackButton onBack={() => navigate('/')}/>
+        <CardExpanded book={books.find((book) => book.isbn === isbn)}/>
       </Container>
       <Footer/>
     </div>    
   );
 }
+
+const mapState = state => ({
+  books: state.books,
+});
+
+export default connect(mapState)(Book);
