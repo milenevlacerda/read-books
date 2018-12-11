8809bbc63bc72d57b00544e64273678c47df4c6c
Milene Vieira Lacerda <milenelacerda@icloud.com>
2018-12-11 10:18:34 -0200

    Add styles to component

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
-  width: 1090px;
+  width: 1200px;
+  flex-wrap: wrap;
+  display: flex;
   position: relative;
   margin: 0 auto;
   padding: 0 ${({ theme }) => theme.container.gutter};
