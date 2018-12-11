f010814ba072668dbdf76d0ab940175fc0051a7f
Milene Vieira Lacerda <milenelacerda@icloud.com>
2018-12-11 10:20:03 -0200

    Add repetition structure

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { navigate } from '@reach/router';
 
 import Container from '@/components/Container';
 import Head from '@/components/Head';
@@ -6,15 +8,21 @@ import Header from '@/components/Header';
 import Card from '@/components/Card';
 import Footer from '@/components/Footer';
 
-export default function Home() {
-  return (
-    <div>
-      <Header/>
-      <Container>
-        <Head title="Home" />
-        <Card/>
-      </Container>
-      <Footer/>
-    </div>    
-  );
-}
+const Home = props => (
+  <div>
+    <Header />
+    <Container>
+      <Head title="Home" />
+      {props.books.map(book => {
+        return <Card book={book} key={book.objectId} onClick={(isbn) => navigate(`/book/${isbn}`)}/>;
+      })}
+    </Container>
+    <Footer />
+  </div>
+);
+
+const mapState = state => ({
+  books: state.books,
+});
+
+export default connect(mapState)(Home);
