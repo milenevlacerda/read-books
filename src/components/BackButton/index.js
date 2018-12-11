51220d078a8f8d0ce207ab1fcdf5ea7cedf40192
Milene Vieira Lacerda <milenelacerda@icloud.com>
2018-12-11 10:14:30 -0200

    Add back-button click function

--- src/components/BackButton/index.js
+++ src/components/BackButton/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import BackArrow from '@/assets/images/back-arrow.png';
 
-export default function() {
+export default function({ onBack }) {
   return (
-    <button class="back-button">
+    <button class="back-button" onClick={() => onBack()}>
       <img src={BackArrow} alt="" className="back-arrow"/>
       <span>Voltar</span>  
     </button>
