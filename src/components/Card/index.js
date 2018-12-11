d4e717648c08aee03da06c1844456bcd522bedf3
Milene Vieira Lacerda <milenelacerda@icloud.com>
2018-12-11 10:18:06 -0200

    Add card click action and repetition structure to component

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import CapaLivro from '@/assets/images/2018.10-aberto-ineditos.jpg';
 import LogoTag from '@/assets/images/logo.png';
 import LogoGoodReads from '@/assets/images/goodreads.png';
 import Rating from '@/assets/images/rating.png';
+import RatingColored from '@/assets/images/rating-colored.png';
 
-export default function() {
-  return (
-    <div className="cards-box">
+export default function({ book, onClick }) {
+  return <div className="cards-box" onClick={() => onClick(book.isbn)}>
       <div className="card">
         <div className="card-header">
-          <img className="book-image" src={CapaLivro} alt="" />
+          <img className="book-image" src={book.cover.url} alt="" />
         </div>
 
         <div className="card-content">
-          <h3 className="card-title">A Filha do Rei do Pântano</h3>
+          <h3 className="card-title">{book.name}</h3>
           <p className="card-description">
-            <strong className="description-item">Autora </strong>
-            Keren Dionne
+            <strong className="description-item">Autor </strong>
+            {book.author}
           </p>
           <p className="card-description">
             <strong className="description-item">Média das Avaliações</strong>
@@ -24,17 +23,26 @@ export default function() {
           <div className="evaluation-box">
             <div className="box">
               <img src={LogoTag} alt="" className="logo" />
-              <span className="grade">5,0</span>
-              <img className="rating" src={Rating} alt=""/>
+              <span className="grade">
+                {(book.totalRatings / book.numRatings).toFixed(1)}
+              </span>
+              <div className="rating">
+                <div className="stars"></div>
+                <div className="stars"></div>
+                {/* <img src={Rating} alt="" /> */}
+                {/* <img src={RatingColored} alt="" width={`${((book.totalRatings / book.numRatings) * 100) / 5}%`} /> */}
+              </div>
             </div>
-            <div className="box">
-              <img src={LogoGoodReads} alt="" className="logo" />
-              <span className="grade">5,0</span>
-              <img className="rating" src={Rating} alt="" />
-            </div>
+            {book.goodreads ? <div className="box">
+                <img src={LogoGoodReads} alt="" className="logo" />
+
+                <span className="grade">
+                  {Number(book.goodreads.average_rating).toFixed(1)}
+                </span>
+                <img className="rating" src={Rating} alt="" />
+              </div> : null}
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    </div>;
+}
